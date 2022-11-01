acfecab31170c170462b46217e2939585b46db7e
totom-kz <115086581+totom-kz@users.noreply.github.com>
2022-11-01 20:31:47 +0100

    Add files via upload

--- beerdata.sql
+++ beerdata.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 01 nov. 2022 à 16:04
--- Version du serveur : 10.4.24-MariaDB
--- Version de PHP : 8.1.6
+-- Généré le : mar. 01 nov. 2022 à 20:30
+-- Version du serveur : 10.4.25-MariaDB
+-- Version de PHP : 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,23 +28,34 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `beerinfo` (
-  `id` int NOT NULL,
+  `Id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `degree` float NOT NULL,
   `type` text NOT NULL,
-  `IBU` int NOT NULL
+  `IBU` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `beerinfo`
 --
 
-INSERT INTO `beerinfo` (`id`, `name`, `degree`, `type`, `IBU`) VALUES
+INSERT INTO `beerinfo` (`Id`, `name`, `degree`, `type`, `IBU`) VALUES
 (1, 'Leffe', 5, 'blonde', 63),
 (2, 'goudale', 7.2, 'blonde', 42),
 (3, 'Chouffe', 8, 'ambrée', 57),
 (4, 'Mont Blanc', 5.8, 'brune', 38),
 (6, 'Heineken', 5, 'blonde', 50);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `beer_taste`
+--
+
+CREATE TABLE `beer_taste` (
+  `id_beer` int(11) NOT NULL,
+  `id_taste` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -60,8 +71,6 @@ CREATE TABLE `comment` (
   `description` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
 --
 -- Déchargement des données de la table `comment`
 --
@@ -70,17 +79,19 @@ INSERT INTO `comment` (`id_biere`, `id_user`, `rating`, `picture`, `description`
 (1, 1, 3, NULL, 'Très bon'),
 (2, 1, 2, NULL, 'Pas mal mais trop mousseuse'),
 (3, 1, 0, NULL, 'On dirait de la pisse'),
-(4, 1, 5, NULL, "J'ai bandé juste en l'ouvrant"),
+(4, 1, 5, NULL, 'J\'ai bandé juste en l\'ouvrant'),
 (6, 1, 3, NULL, 'Je préfere le cidre'),
 (2, 3, 4, NULL, 'Une des meilleures');
+
+-- --------------------------------------------------------
 
 --
 -- Structure de la table `follow`
 --
 
 CREATE TABLE `follow` (
-  `iduser` int NOT NULL,
-  `iduserfollow` int NOT NULL
+  `iduser` int(11) NOT NULL,
+  `iduserfollow` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -91,10 +102,21 @@ CREATE TABLE `follow` (
 
 CREATE TABLE `logins` (
   `idlogins` int(11) NOT NULL,
-  `username` VARCHAR(100) NOT NULL,
-  `email` VARCHAR(200) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(200) NOT NULL,
   `password` text NOT NULL,
   `iduser` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `taste`
+--
+
+CREATE TABLE `taste` (
+  `name` varchar(100) NOT NULL,
+  `description` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -105,10 +127,10 @@ CREATE TABLE `logins` (
 
 CREATE TABLE `user` (
   `iduser` int(11) NOT NULL,
-  `username` VARCHAR(100) NOT NULL,
-  `f_name` VARCHAR(100) NOT NULL,
-  `name` VARCHAR(100) NOT NULL,
-  `email` VARCHAR(200) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `f_name` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(200) NOT NULL,
   `birthdate` date NOT NULL,
   `password` text NOT NULL,
   `datecreation` date NOT NULL DEFAULT current_timestamp()
@@ -133,12 +155,17 @@ INSERT INTO `user` (`iduser`, `username`, `f_name`, `name`, `email`, `birthdate`
 ALTER TABLE `beerinfo`
   ADD PRIMARY KEY (`Id`);
 
-
 --
 -- Index pour la table `logins`
 --
 ALTER TABLE `logins`
   ADD PRIMARY KEY (`idlogins`);
+
+--
+-- Index pour la table `taste`
+--
+ALTER TABLE `taste`
+  ADD PRIMARY KEY (`name`);
 
 --
 -- Index pour la table `user`
