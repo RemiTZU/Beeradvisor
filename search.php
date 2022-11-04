7d0aa449842ff7dd3cc860368a6399db07ef0787
totom-kz <115086581+totom-kz@users.noreply.github.com>
2022-11-04 11:19:34 +0100

    Update search.php

--- search.php
+++ search.php
@@ -29,6 +29,29 @@
             IBU :
             <input type="checkbox" class="IBU" name="bool_IBU" id="bool_IBU" onchange="cb_clique(event)">
             <input type="number" name="IBU" id="IBU" min="0" max="100" value="30"> <br>
+            Goûts :
+            <select name="taste" id="taste" onchange="taste_select()">
+                <option value="reset">qu'importe</option>                
+                <option value="cannelle">cannelle</option>
+                <option value="agrume">agrume</option>
+                <option value="vanille">vanille</option>
+                <option value="fruits rouges">fruits rouges</option>
+            </select>
+            -> 
+            <input type="text" name="taste_txt" id="taste_txt" readonly> <br>
+
+            <script>
+                function taste_select() {
+                    var taste = document.getElementById("taste").value;
+                    var x = document.getElementById("taste_txt").value;
+                    if (x.indexOf("taste") == -1) {
+                        document.getElementById("taste_txt").value = x + taste + ";";
+                    }
+                    if (taste == "reset") {
+                        document.getElementById("taste_txt").value = "";
+                    }
+                }
+            </script>
 
             <input type="submit" value="valider">
         </form>
@@ -46,14 +69,29 @@
         $alcohol = $_POST["alcohol"];
         $IBU = $_POST["IBU"];
 
+        $list_taste = explode(";", $_POST["taste_txt"]);
+
+        echo json_encode($list_taste). "<br>";
+
+        $taste = $_POST["taste"];
+
         $req = "SELECT * FROM beerinfo";
-        $where = "";
+        $where = " WHERE TRUE ";
         $order = "";
         $array = array();
         if ($_POST["category"] != "tout") {
-            $req = $req . " WHERE type=? ";
+            //$req = $req . " WHERE type=? ";
+            $where = $where . " AND type=? ";
             $array = array_merge($array, array($type));
         }
+
+        $i = 0;
+        while ($list_taste[$i]) {
+            $where = $where . " AND name IN (SELECT beerinfo.name FROM beer_taste INNER JOIN beerinfo ON beer_taste.id_beer = beerinfo.id INNER JOIN taste ON beer_taste.taste_name = taste.name WHERE taste.name = ?)";
+            $array = array_merge($array, array($list_taste[$i]));
+            $i ++;
+        }
+
         if (isset($_POST["bool_alcohol"])) {
             $order = $order . " abs(degree-?)/".ecart_type("degree", "beerinfo", $db)." + ";
             $array = array_merge($array, array($alcohol));
@@ -65,9 +103,9 @@
         if ($order != "") {
             $order = " ORDER BY (" . $order . " 0)";
         }
-        echo $req . $order . " comportant comme argument : " . json_encode($array) . "<br>";
+        echo $req . $where . $order . " comportant comme argument : " . json_encode($array) . "<br>";
         
-        $query = $db->prepare($req . $order);
+        $query = $db->prepare($req . $where . $order);
         $res = $query->execute($array);
         $data = $query->fetch();
 
