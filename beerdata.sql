3176c58421ff44c05eded87705ddba598845b994
totom-kz <115086581+totom-kz@users.noreply.github.com>
2022-11-07 18:22:24 +0100

    Update beerdata.sql

--- beerdata.sql
+++ beerdata.sql
@@ -2,10 +2,10 @@
 -- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le : lun. 07 nov. 2022 à 17:41
--- Version du serveur : 10.4.24-MariaDB
--- Version de PHP : 8.1.6
+-- Host: 127.0.0.1
+-- Generation Time: Nov 07, 2022 at 06:19 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,17 +18,17 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `beerdata`
+-- Database: `beerdata`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `beerinfo`
+-- Table structure for table `beerinfo`
 --
 
 CREATE TABLE `beerinfo` (
-  `id` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `degree` float NOT NULL,
   `type` text NOT NULL,
@@ -36,20 +36,24 @@ CREATE TABLE `beerinfo` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Déchargement des données de la table `beerinfo`
+-- Dumping data for table `beerinfo`
 --
 
-INSERT INTO `beerinfo` (`id`, `name`, `degree`, `type`, `IBU`) VALUES
-(1, 'Leffe', 5, 'blonde', 63),
-(2, 'goudale', 7.2, 'blonde', 42),
-(3, 'Chouffe', 8, 'ambrée', 57),
-(4, 'Mont Blanc', 5.8, 'brune', 38),
-(6, 'Heineken', 5, 'blonde', 50);
+INSERT INTO `beerinfo` (`Id`, `name`, `degree`, `type`, `IBU`) VALUES
+(1, 'Leffe Blonde', 6.6, 'Lager', 38),
+(2, 'Goudale Blonde', 7.2, 'Lager', 51),
+(3, 'Chouffe Blonde', 8, 'Lager', 67),
+(6, 'Heineken', 5, 'Lager', 50),
+(7, 'Bête ambrée', 8, 'Lager', 33),
+(14, 'Goudale Ambrée', 7.2, 'Brown Ale', 60),
+(15, 'Goudale IPA', 7, 'IPA', 0),
+(16, 'Goudale IPA', 7.2, 'IPA', 64),
+(17, 'Mont Blanc - La Bleue', 5.8, '', 38);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `beer_taste`
+-- Table structure for table `beer_taste`
 --
 
 CREATE TABLE `beer_taste` (
@@ -57,10 +61,48 @@ CREATE TABLE `beer_taste` (
   `id_taste` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `beer_taste`
+--
+
+INSERT INTO `beer_taste` (`id_beer`, `id_taste`) VALUES
+(7, 1),
+(7, 2),
+(1, 4),
+(1, 5),
+(1, 3),
+(14, 6),
+(14, 7),
+(14, 8),
+(14, 1),
+(15, 9),
+(15, 10),
+(17, 11),
+(6, 3);
+
 -- --------------------------------------------------------
 
 --
--- Structure de la table `comment`
+-- Table structure for table `beer_type`
+--
+
+CREATE TABLE `beer_type` (
+  `name` varchar(100) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `beer_type`
+--
+
+INSERT INTO `beer_type` (`name`, `description`) VALUES
+('API', 'Originally, India Pale Ale or IPA was a British pale ale brewed with extra hops. High levels of this bittering agent made the beer stable enough to survive the long boat trip to India without spoiling. The extra dose of hops gives IPA beers their bitter taste. Depending on the style of hops used, IPAs may have fruit-forward citrus flavors or taste of resin and pine.\r\nAmerican brewers have taken the IPA style and run with it, introducing unusual flavors and ingredients to satisfy U.S. beer drinkers\' love for the brew style.'),
+('Lager', 'Lagers are a newer style of beer with two key differences from ales. Lagers ferment for a long time at a low temperature, and they rely on bottom-fermenting yeasts, which sink to the bottom of the fermenting tank to do their magic.\r\nLagers are common among European countries, including Czechia, Germany, and the Netherlands, as well as in Canada, where they make up more than half of all beer sales.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
 --
 
 CREATE TABLE `comment` (
@@ -68,25 +110,26 @@ CREATE TABLE `comment` (
   `id_user` int(11) NOT NULL,
   `rating` int(11) NOT NULL,
   `picture` blob DEFAULT NULL,
-  `description` text DEFAULT NULL
+  `description` text DEFAULT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Déchargement des données de la table `comment`
+-- Dumping data for table `comment`
 --
 
-INSERT INTO `comment` (`id_biere`, `id_user`, `rating`, `picture`, `description`) VALUES
-(1, 1, 3, NULL, 'Très bon'),
-(2, 1, 2, NULL, 'Pas mal mais trop mousseuse'),
-(3, 1, 0, NULL, 'On dirait de la pisse'),
-(4, 1, 5, NULL, 'J\'ai bandé juste en l\'ouvrant'),
-(6, 1, 3, NULL, 'Je préfere le cidre'),
-(2, 3, 4, NULL, 'Une des meilleures');
+INSERT INTO `comment` (`id_biere`, `id_user`, `rating`, `picture`, `description`, `date`) VALUES
+(1, 1, 3, NULL, 'Très bon', '2022-11-06'),
+(2, 1, 2, NULL, 'Pas mal mais trop mousseuse', '2022-11-06'),
+(3, 1, 0, NULL, 'On dirait de la pisse', '2022-11-06'),
+(4, 1, 5, NULL, 'J\'ai bandé juste en l\'ouvrant', '2022-11-06'),
+(6, 1, 3, NULL, 'Je préfere le cidre', '2022-11-06'),
+(2, 3, 4, NULL, 'Une des meilleures', '2022-11-06');
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `follow`
+-- Table structure for table `follow`
 --
 
 CREATE TABLE `follow` (
@@ -98,40 +141,50 @@ CREATE TABLE `follow` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `logins`
+-- Table structure for table `logins`
 --
 
 CREATE TABLE `logins` (
   `idlogins` int(11) NOT NULL,
-  `username` text NOT NULL,
-  `email` text NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(200) NOT NULL,
   `password` text NOT NULL,
-  `adminstate` tinyint(1) NOT NULL
+  `iduser` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Déchargement des données de la table `logins`
---
-
-INSERT INTO `logins` (`idlogins`, `username`, `email`, `password`, `adminstate`) VALUES
-(8, 'ee', 'remi.bonnet@utbm.fr', '$argon2id$v=19$m=65536,t=4,p=1$TnpqZ2ZDSEd0Q0N0d0Uvaw$Os7oTYjv+n2ZpmLht4UJyikV1QVGLea+Z/bqv1qWKk0', 0),
-(9, 'admin', 'bonnetremi74@gmail.com', '$argon2id$v=19$m=65536,t=4,p=1$bU1wdFJWd09SVmpiQ3dPaQ$ZG0wX8rsgKGZzt0cPWwuXBhTdrChIk7yZbKjGSN56kE', 1);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `taste`
+-- Table structure for table `taste`
 --
 
 CREATE TABLE `taste` (
+  `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `description` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `taste`
+--
+
+INSERT INTO `taste` (`id`, `name`, `description`) VALUES
+(9, 'Agrume', NULL),
+(7, 'Café', NULL),
+(1, 'Caramel', NULL),
+(6, 'Chocolat', NULL),
+(5, 'Clou de girofle', NULL),
+(3, 'Fruité', 'abondance de saveur de fruits'),
+(10, 'Fruits exotiques', NULL),
+(11, 'Myrtille', ''),
+(8, 'Noisette', NULL),
+(2, 'Pain d\'épice', 'Miel, cannelle, moelleux et parfumé'),
+(4, 'Vanille', NULL);
+
 -- --------------------------------------------------------
 
 --
--- Structure de la table `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -142,7 +195,7 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='table information utilisateur';
 
 --
--- Déchargement des données de la table `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`iduser`, `f_name`, `name`, `birthdate`) VALUES
@@ -154,63 +207,63 @@ INSERT INTO `user` (`iduser`, `f_name`, `name`, `birthdate`) VALUES
 (11, 'admin', 'admin', '2003-11-18');
 
 --
--- Index pour les tables déchargées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `beerinfo`
+-- Indexes for table `beerinfo`
 --
 ALTER TABLE `beerinfo`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`Id`);
 
 --
--- Index pour la table `follow`
+-- Indexes for table `follow`
 --
 ALTER TABLE `follow`
   ADD PRIMARY KEY (`id`);
 
 --
--- Index pour la table `logins`
+-- Indexes for table `logins`
 --
 ALTER TABLE `logins`
   ADD PRIMARY KEY (`idlogins`);
 
 --
--- Index pour la table `taste`
+-- Indexes for table `taste`
 --
 ALTER TABLE `taste`
   ADD PRIMARY KEY (`name`);
 
 --
--- Index pour la table `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`iduser`);
 
 --
--- AUTO_INCREMENT pour les tables déchargées
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `beerinfo`
+-- AUTO_INCREMENT for table `beerinfo`
 --
 ALTER TABLE `beerinfo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
--- AUTO_INCREMENT pour la table `follow`
+-- AUTO_INCREMENT for table `follow`
 --
 ALTER TABLE `follow`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pour la table `logins`
+-- AUTO_INCREMENT for table `logins`
 --
 ALTER TABLE `logins`
-  MODIFY `idlogins` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idlogins` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT pour la table `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `iduser` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
